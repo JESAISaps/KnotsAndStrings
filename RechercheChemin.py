9c982a9a8fa0bd5d267b1908d150ca1ca2c3b43d
JESAISap <nicolas.strekowski@gmail.com>
2024-12-10 20:38:21 +0100

    started to add thins to main

--- RechercheChemin.py
+++ RechercheChemin.py
@@ -1,6 +1,9 @@
 from utils import ASSODIRECTIONNOMBRE, accessiblechemins, G
 
 def ReconstruireChemin(depart, arrivee, arcs):
+    """
+    Retourne le chemin de depart a arrivee sous la forme de succession de sommets
+    """
     chemin = [arrivee]
     dicoArcs = dict(arcs)
     #print(f"Arcs: {arcs}")
@@ -11,7 +14,22 @@ def ReconstruireChemin(depart, arrivee, arcs):
 
     return chemin
 
+def ReconstruireCheminAsTuples(depart, arrivee, arcs) -> list[tuple[int, int]]:
+    """
+    Retourne le chemin de depart a arrivée sous la formes d'arcs. (enleve les arcs inutiles)
+    arcs de la forme [(sommet : provenance)]
+    """
+    dico = dict(arcs)
+    rep = [(dico[arrivee], arrivee)]
+
+    while rep[0][0] != depart:
+        rep.insert(0,(dico[rep[0][0]], rep[0][0]))
+    return rep
+
 def CheminToGo(G, d, a):
+    """
+    Retourne la liste des cles des sommets du graph G pour aller du sommet d au sommet a
+    """
     accessibleVerticises, links = accessiblechemins(G, d)
     #print(accessibleVerticises)
     if a not in accessibleVerticises:
@@ -39,9 +57,11 @@ def test():
     arrivee = 6
     #print(CheminToGo(G, depart, arrivee))
     #PrintCreateGPS(G, CheminToGo(G, depart, arrivee))
-    print(CreateGPS(G, CheminToGo(G, depart, arrivee)))
+    #print(CreateGPS(G, CheminToGo(G, depart, arrivee)))
     flag = flag and CheminToGo(G, depart, arrivee)
     return flag
 
 if __name__ == "__main__":
     assert test()
+    print(dict(accessiblechemins(G, 0)[1]))
+    print(ReconstruireCheminAsTuples(0,6,accessiblechemins(G, 0)[1]))
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import doctest
 import json
+import webbrowser
+from utils import GetGraphInData, DOTPATH
+from CreationCarte import CreerCarteAvecChemin, CreerCarteVisite
+from SortieRatee import GetImpossibleExits
+import graphviz
 
-doctest.testfile("doctest.txt")
-with open("./data/data.json", "r") as jsonFile:
-    G = json.load(jsonFile)
-print(G["H"])
+
+
+def ShowLabyrinth(name, start=None, end=None):
+    """
+    Crée le dot du labyrinthe et l'affiche.
+    Si start et end sont donnés, alors on met le chemin en rouge.
+    """
+
+    graph = GetGraphInData(name)
+
+    with open(DOTPATH, "w") as file:
+        match start,end:
+            case None, None:
+                CreerCarteVisite(graph, file)
+            case int(), int():
+                CreerCarteAvecChemin(graph, file, start, end)
+            case _:
+                raise AttributeError("Il faut donner un depart et une fin, ou rien du tout.")
+    
+    graphviz.render("dot", "pdf", DOTPATH)
+    webbrowser.open(DOTPATH + ".pdf", 2)
+
+
+if __name__ == "__main__":
+    doctest.testfile("doctest.txt")
+    g = GetGraphInData("g")
+    print(GetImpossibleExits(g))
+    ShowLabyrinth("g", 0, 6)
